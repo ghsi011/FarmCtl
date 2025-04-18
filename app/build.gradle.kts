8ed002d9b32ec39d57c865ddd32c0edc74f47e82
ghsi011 <ghsi011@gmail.com>
2025-04-18 19:40:55 +0300

    build works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,11 @@ android {
         kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
